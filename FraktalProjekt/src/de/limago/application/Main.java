3df997776586b584ac7661a10d871a45a253a4c0
Jo Wagner <jowagner@limago.de>
2025-02-09 10:39:42 +0100

    gitignore updated

---
+++ FraktalProjekt/src/de/limago/application/Main.java
@@ -0,0 +1,9 @@
+package de.limago.application;
+
+import de.limago.gui.MandelbrotView;
+
+public class Main {
+    public static void main(String[] args) {
+        new MandelbrotView().setVisible(true);
+    }
+}
